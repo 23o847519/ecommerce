f1130c38a922081bd406a660afe2472af8bcc1ca
23840932 <violetpayne11@gmail.com>
2022-05-29 00:00:32 -0500

    s
    end on details

--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -8,6 +8,31 @@ fields: [
     title: 'Image',
     type: 'array',
     of: [{type: 'image'}],
-}
-]
+    options: {
+        hotspot: true,
+    }
+ },
+   {
+    name: 'name',
+    title: 'Name',
+    tye: 'string',
+   },
+   {
+    name: 'slug',
+    title: 'Slug',
+    type: 'Slug',
+    options: {
+        source: 'name',
+        maxLength: 90,
+    }
+  },
+  {
+      name: 'price',
+      title: 'price',
+      type: 'number',
+  },
+  {
+    name: 'details'
+  },
+ ]
 }
